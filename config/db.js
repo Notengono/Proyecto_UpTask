db62ba7743e8f164dd1209ce95393f2946dc8d51
Notengono <68205330+Notengono@users.noreply.github.com>
2021-06-14 21:45:49 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -14,8 +14,8 @@ require('dotenv').config({ path: 'variables.env' })
 
 // Option 2: Passing parameters separately (other dialects)
 const db = new Sequelize(process.env.BD_NOMBRE,
-    process.env.BS_USER,
-    process.env.BS_PASS,
+    process.env.BD_USER,
+    process.env.BD_PASS,
     {
         host: process.env.BD_HOST,
         dialect: 'mysql',
